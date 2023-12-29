d67b70684a72780228ac6056b58469557afb6b06
MD AKHTAR <155041297+akhtarsru@users.noreply.github.com>
2023-12-29 09:21:57 +0000

    t3

--- t3.java
+++ t3.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
+
+public class t3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get input from the user
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is prime
+        boolean isPrime = checkPrime(number);
+
+        // Display the result
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+
+    // Function to check if a number is prime
+    private static boolean checkPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
